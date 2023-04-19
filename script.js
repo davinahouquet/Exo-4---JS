b2e7b8a87bb81812f977fc2a04696e8578f90e90
davinahouquet <davina.houquet@hotmail.com>
2023-04-19 16:34:12 +0200

    Création carre + random color

---
+++ script.js
@@ -0,0 +1,11 @@
+const carre = document.getElementById('carre');
+carre.style.backgroundColor = getRandomColor();
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
