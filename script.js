02bd6da41e78a233b882e529cc548945882515ee
davinahouquet <davinahouquet@hotmail.fr>
2023-04-20 16:57:36 +0200

    Fin d'exercice

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const carre = document.getElementById("carre");
-carre.style.backgroundColor = getCouleurAleatoire();
+
 //Fonction pour générer une couleur aléatoire : utilise code hexadecima d'ou les chiffres et lettres + #)
 function getCouleurAleatoire() {
   const letters = '0123456789ABCDEF';
@@ -11,31 +10,38 @@ function getCouleurAleatoire() {
 }
 
 //Ajouter(add) et supprimer (remove) carre             
-document.addEventListener("keydown", function(touche) { //(event) non plus! Il stocke juste les informations de la fonctions et on      vérifie après avec  === "ArrowDown"(qui correspond à la flèche du bas du clavier)
+document.addEventListener("keydown", function (touche) { //(event) non plus! Il stocke juste les informations de la fonctions et on      vérifie après avec  === "ArrowDown"(qui correspond à la flèche du bas du clavier)
 
   const carres = document.querySelectorAll(".carre"); //On délcare carres au début de la fonction pour qu'elle soit accesible par tous les if
-  
-  if(touche.key === "ArrowDown") {  
 
-    //const pour créer créer nvx carres
+
+  if (touche.key === "ArrowDown") {
+    if (carres.length <= 224) {
+      console.log("test")
+      //const pour créer créer nvx carres
       const newCarre = document.createElement("button");
       newCarre.classList.add("carre");
       newCarre.style.backgroundColor = getCouleurAleatoire();
-      
-      newCarre.addEventListener("click", function() {
-        if(carres.length){
-        newCarre.style.backgroundColor = "black";
+
+      newCarre.addEventListener("click", function () {
+        const carresReload = document.querySelectorAll(".carre");
+
+        console.log(carres.length)
+        if (carresReload.length > 2) {
+          newCarre.style.backgroundColor = "black";
         }
       });
-
-      document.querySelector("div").appendChild(newCarre);
-console.log(newCarre)
-    } else if (touche.key === "ArrowUp"){
-
-        if(carres.length > 0){
-            //const qui prend toute la longueur actuelle des carres et en enlève juste 1 par action
-            const lastCarre = carres[carres.length - 1];
-            lastCarre.remove();
-        }
+      document.querySelector('.container').appendChild(newCarre)
     }
-  });
+  }
+
+
+  else if (touche.key === "ArrowUp") {
+
+    if (carres.length > 0) {
+      //const qui prend toute la longueur actuelle des carres et en enlève juste 1 par action
+      const lastCarre = carres[carres.length - 1];
+      lastCarre.remove();
+    }
+  }
+});
