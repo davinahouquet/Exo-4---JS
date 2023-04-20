546d651d35bb2ef375fe9a6ba32a5a9cf91b8657
davinahouquet <davinahouquet@hotmail.fr>
2023-04-20 11:47:54 +0200

    Carres -> buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const carre = document.getElementById('carre');
 carre.style.backgroundColor = getCouleurAleatoire();
 
-//Fonction pour générer une couleur aléatoire
+//Fonction pour générer une couleur aléatoire : utilise code hexadecima d'ou les chiffres et lettres + #)
 function getCouleurAleatoire() {
   const letters = '0123456789ABCDEF';
   let color = '#';
@@ -11,17 +11,35 @@ function getCouleurAleatoire() {
   return color;
 }
 
+function colorBlack(){
+  const letters = '000000';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 6)];
+  }
+  return color;  
+}
+
 //Ajouter(add) et supprimer (remove) carre
 document.addEventListener("keydown", function(event) {
-    if (event.keyCode === 40) {  //40 et le num de la touche down du clavier
+  
+  if (event.key === "ArrowDown") {  //40 et le num de la touche down du clavier
 
     //const pour créer créer nvx carres
-      const newCarre = document.createElement("div");
+      const newCarre = document.createElement("button");
       newCarre.classList.add("carre");
       newCarre.style.backgroundColor = getCouleurAleatoire();
-      document.querySelector(".container").appendChild(newCarre);
+      
+      newCarre.addEventListener("click", () => {
+        if(newCarre.length > 225){
+          newCarre.onclick = colorBlack();
+        }
+      });
+    
 
-    } else if (event.keyCode === 38){
+      document.querySelector(".container").appendChild(newCarre);
+console.log(newCarre)
+    } else if (event.key === "ArrowUp"){
 
         const carres = document.querySelectorAll(".carre");
         if(carres.length > 0){
@@ -31,5 +49,3 @@ document.addEventListener("keydown", function(event) {
         }
     }
   });
-  
-  //Fonction qui genere couleur aleatoire : utilise code hexadecima d'ou les chiffres et lettres + #)
