d202a58ec0af96b5eb617f1e0845110152886bb4
davinahouquet <davinahouquet@hotmail.fr>
2023-04-20 14:47:56 +0200

    Couleur (noir) au clique : OK

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const carre = document.getElementById('carre');
+const carre = document.getElementById("carre");
 carre.style.backgroundColor = getCouleurAleatoire();
-
 //Fonction pour générer une couleur aléatoire : utilise code hexadecima d'ou les chiffres et lettres + #)
 function getCouleurAleatoire() {
   const letters = '0123456789ABCDEF';
@@ -11,37 +10,28 @@ function getCouleurAleatoire() {
   return color;
 }
 
-function colorBlack(){
-  const letters = '000000';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 6)];
-  }
-  return color;  
-}
+//Ajouter(add) et supprimer (remove) carre             
+document.addEventListener("keydown", function(touche) { //(event) non plus! Il stocke juste les informations de la fonctions et on      vérifie après avec  === "ArrowDown"(qui correspond à la flèche du bas du clavier)
 
-//Ajouter(add) et supprimer (remove) carre
-document.addEventListener("keydown", function(event) {
+  const carres = document.querySelectorAll(".carre"); //On délcare carres au début de la fonction pour qu'elle soit accesible par tous les if
   
-  if (event.key === "ArrowDown") {  //40 et le num de la touche down du clavier
+  if(touche.key === "ArrowDown") {  
 
     //const pour créer créer nvx carres
       const newCarre = document.createElement("button");
       newCarre.classList.add("carre");
       newCarre.style.backgroundColor = getCouleurAleatoire();
       
-      newCarre.addEventListener("click", () => {
-        if(newCarre.length > 225){
-          newCarre.onclick = colorBlack();
+      newCarre.addEventListener("click", function() {
+        if(carres.length){
+        newCarre.style.backgroundColor = "black";
         }
       });
-    
 
-      document.querySelector(".container").appendChild(newCarre);
+      document.querySelector("div").appendChild(newCarre);
 console.log(newCarre)
-    } else if (event.key === "ArrowUp"){
+    } else if (touche.key === "ArrowUp"){
 
-        const carres = document.querySelectorAll(".carre");
         if(carres.length > 0){
             //const qui prend toute la longueur actuelle des carres et en enlève juste 1 par action
             const lastCarre = carres[carres.length - 1];
